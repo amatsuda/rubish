c85452d55a4749e629ac3fe14f6b6d8702b68ebc
Akira Matsuda <ronnie@dio.jp>
2026-01-09 11:23:25 +0900

    Implement compat54 shopt option
    compat54: bash 5.4 compatibility mode
    
    - shopt -s compat54 enables bash 5.4 compatibility mode
    - Like other compat options, enabling one automatically disables all others (mutually exclusive)
    - shopt -u compat54 disables it
    - shopt -q compat54 queries the status

---
+++ test/test_compat54.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+class TestCompat54 < Test::Unit::TestCase
+  def setup
+    @repl = Rubish::REPL.new
+    @original_shell_options = Rubish::Builtins.shell_options.dup
+    @tempdir = Dir.mktmpdir('rubish_compat54_test')
+    @original_dir = Dir.pwd
+    Dir.chdir(@tempdir)
+  end
+
+  def teardown
+    Dir.chdir(@original_dir)
+    Rubish::Builtins.shell_options.clear
+    @original_shell_options.each { |k, v| Rubish::Builtins.shell_options[k] = v }
+    FileUtils.rm_rf(@tempdir)
+  end
+
+  # compat54 tests
+  def test_compat54_disabled_by_default
+    assert_false Rubish::Builtins.shopt_enabled?('compat54')
+  end
+
+  def test_compat54_can_be_enabled
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+  end
+
+  def test_compat54_can_be_disabled
+    execute('shopt -s compat54')
+    execute('shopt -u compat54')
+    assert_false Rubish::Builtins.shopt_enabled?('compat54')
+  end
+
+  def test_compat54_in_shell_options
+    assert Rubish::Builtins::SHELL_OPTIONS.key?('compat54')
+    assert_equal false, Rubish::Builtins::SHELL_OPTIONS['compat54'][0]
+  end
+
+  def test_compat54_in_compat_options
+    assert_includes Rubish::Builtins::COMPAT_OPTIONS, 'compat54'
+  end
+
+  # Test compat options are mutually exclusive
+  def test_compat54_disables_compat53
+    execute('shopt -s compat53')
+    assert Rubish::Builtins.shopt_enabled?('compat53')
+
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+    assert_false Rubish::Builtins.shopt_enabled?('compat53')
+  end
+
+  def test_compat53_disables_compat54
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+
+    execute('shopt -s compat53')
+    assert Rubish::Builtins.shopt_enabled?('compat53')
+    assert_false Rubish::Builtins.shopt_enabled?('compat54')
+  end
+
+  def test_compat54_disables_other_compat_options
+    execute('shopt -s compat50')
+    assert Rubish::Builtins.shopt_enabled?('compat50')
+
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+    assert_false Rubish::Builtins.shopt_enabled?('compat50')
+  end
+
+  # Test shopt output for compat54
+  def test_shopt_print_shows_compat54
+    output = capture_output do
+      execute('shopt compat54')
+    end
+    assert_match(/compat54/, output)
+    assert_match(/off/, output)
+
+    execute('shopt -s compat54')
+
+    output = capture_output do
+      execute('shopt compat54')
+    end
+    assert_match(/compat54/, output)
+    assert_match(/on/, output)
+  end
+
+  # Test shopt -q for compat54
+  def test_shopt_q_compat54
+    result = Rubish::Builtins.run('shopt', ['-q', 'compat54'])
+    assert_false result
+
+    Rubish::Builtins.run('shopt', ['-s', 'compat54'])
+    result = Rubish::Builtins.run('shopt', ['-q', 'compat54'])
+    assert result
+  end
+
+  # Test toggle behavior
+  def test_toggle_compat54
+    # Default: disabled
+    assert_false Rubish::Builtins.shopt_enabled?('compat54')
+
+    # Enable
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+
+    # Disable
+    execute('shopt -u compat54')
+    assert_false Rubish::Builtins.shopt_enabled?('compat54')
+
+    # Re-enable
+    execute('shopt -s compat54')
+    assert Rubish::Builtins.shopt_enabled?('compat54')
+  end
+
+  # Test that the option is listed in shopt output
+  def test_compat54_in_shopt_list
+    output = capture_output do
+      execute('shopt')
+    end
+    assert_match(/compat54/, output)
+  end
+
+  # Test all compat options are present including compat54
+  def test_all_compat_options_exist
+    expected = %w[compat10 compat31 compat32 compat40 compat41 compat42 compat43 compat44 compat50 compat51 compat52 compat53 compat54]
+    expected.each do |opt|
+      assert Rubish::Builtins::SHELL_OPTIONS.key?(opt), "Missing option: #{opt}"
+      assert_includes Rubish::Builtins::COMPAT_OPTIONS, opt, "Missing from COMPAT_OPTIONS: #{opt}"
+    end
+  end
+end
